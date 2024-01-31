bc135a0f8547e72f4c3adbc6fdfcf9da9e0af836
Aphex2in <aaoalqurashi@gmail.com>
2024-01-31 03:39:04 -0500

    Initial commit

---
+++ bashrc
@@ -0,0 +1,2 @@
+# ~/dotfiles/bashrc
+export PS1="[\u@\h \W]\$ "
---
+++ install.sh
@@ -0,0 +1,7 @@
+# ~/dotfiles/install.sh
+#!/bin/bash
+
+ln -sf ~/dotfiles/bashrc ~/.bashrc
+# Add more lines for other dotfiles
+
+echo "Dotfiles installed!"
